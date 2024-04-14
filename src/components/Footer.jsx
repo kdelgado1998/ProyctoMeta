cfe88dbce542bbc2bcf77b462b40e09cd1c79f2e
kdelgado <keylordelgado@gmail.com>
2024-04-14 15:52:09 -0600

    first commit

---
+++ src/components/Footer.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+export const Footer = () => {
+    return (
+        <footer className="footer">
+            <div className="contenedor contenido-footer bg-white">
+                <div className="footer-box ubicacion">
+                    <h3>Ubicación</h3>
+                    <p>San José, Costa Rica</p>
+                    <p>C/55, Montes de Oca</p>
+                </div>
+            </div>
+            <p className="copy-right">Todos los derechos reservados META</p>
+        </footer>
+    )
+}
